7e904a3c53d19c4878c21178dce69ff17fb2c931
smithjke <smithjke@gmail.com>
2022-07-12 23:58:18 +0300

    fix: update json-rpc middleware

--- src/app/middlewares/json-rpc-middleware.ts
+++ src/app/middlewares/json-rpc-middleware.ts
@@ -1,23 +1,32 @@
-import { createControllerMethod } from '~/1st-server-api';
+import { createControllerMethod as ccm } from '~/1st-server-api';
 import { createJsonRpcMiddleware } from '~/1st-server-json-rpc';
 import {
   AuthController,
+  RoleController,
   SessionController,
   UserController,
-} from '~/app';
+} from '../controllers';
 
 export const jsonRpcMiddleware = () => createJsonRpcMiddleware({
-  ['auth-login']: createControllerMethod(AuthController, 'login'),
+  ['auth-login']: ccm(AuthController, 'login'),
+  ['auth-init']: ccm(AuthController, 'init'),
 
-  ['session-create']: createControllerMethod(SessionController, 'create'),
-  ['session-update']: createControllerMethod(SessionController, 'update'),
-  ['session-get']: createControllerMethod(SessionController, 'get'),
-  ['session-list']: createControllerMethod(SessionController, 'list'),
-  ['session-delete']: createControllerMethod(SessionController, 'delete'),
+  ['role-create']: ccm(RoleController, 'create'),
+  ['role-update']: ccm(RoleController, 'update'),
+  ['role-get']: ccm(RoleController, 'get'),
+  ['role-list']: ccm(RoleController, 'list'),
+  ['role-delete']: ccm(RoleController, 'delete'),
 
-  ['user-create']: createControllerMethod(UserController, 'create'),
-  ['user-update']: createControllerMethod(UserController, 'update'),
-  ['user-get']: createControllerMethod(UserController, 'get'),
-  ['user-list']: createControllerMethod(UserController, 'list'),
-  ['user-delete']: createControllerMethod(UserController, 'delete'),
+  ['session-create']: ccm(SessionController, 'create'),
+  ['session-update']: ccm(SessionController, 'update'),
+  ['session-get']: ccm(SessionController, 'get'),
+  ['session-list']: ccm(SessionController, 'list'),
+  ['session-delete']: ccm(SessionController, 'delete'),
+  ['session-get-by-token']: ccm(SessionController, 'getByToken'),
+
+  ['user-create']: ccm(UserController, 'create'),
+  ['user-update']: ccm(UserController, 'update'),
+  ['user-get']: ccm(UserController, 'get'),
+  ['user-list']: ccm(UserController, 'list'),
+  ['user-delete']: ccm(UserController, 'delete'),
 });
